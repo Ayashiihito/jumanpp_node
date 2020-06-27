13eff5e23b2970c12afd767dc44d0b25e094728e
vladimir <antsupov.dev@gmail.com>
2020-06-27 20:51:00 +0300

    treat new lines

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ const main = async () => {
   const one = await jumanpp.query('玄関で愛を待ち惚け');
   const two = await jumanpp.query('囁く声で喘いで');
   const three = await jumanpp.query('後悔の悔を教えてほしいわ');
+
   jumanpp.close(); //terminates running jumanpp process
 
   console.log(one);
--- jumanpp.js
+++ jumanpp.js
@@ -6,6 +6,12 @@ class Jumnanpp {
 
   constructor(command = 'jumanpp') {
     this.instance = spawn(command);
+
+    this.instance.on('error', err => {
+      throw new Error(
+        `Failed to start process with provided command: ${command}.`
+      );
+    });
   }
 
   close() {
@@ -13,14 +19,22 @@ class Jumnanpp {
   }
 
   async query(sentence) {
+    if (!sentence) {
+      throw new Error(`Query: '${sentence}' is not a valid query.`);
+    }
+    if (!this.instance) {
+      throw new Error(
+        `Expected and instance of ${command}, but found ${this.instance}`
+      );
+    }
+
+    sentence = sentence.replace(/[\r\n]+/, '');
     this.instance.stdin.write(sentence + EOL);
 
     return new Promise((resolve, reject) => {
-      let response = '';
-
-      this.instance.stdout.on('data', data => {
-        response += data;
-        if (response.includes('EOS')) resolve(response);
+      //no actuall buffer handeling is impelmented
+      this.instance.stdout.on('data', response => {
+        resolve(response.toString());
       });
     });
   }
